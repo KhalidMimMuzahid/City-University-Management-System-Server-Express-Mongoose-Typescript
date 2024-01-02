e26a8d4d9214bfb5416ba7303844a25752b26790
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-01-02 22:15:12 +0600

    global error handling updated

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,7 +2,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
-import { ZodError } from 'zod';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
 
 // if we use ErrorRequestHandler type we no need to declare the type of those {any Request Response NextFunction}
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
@@ -10,7 +12,6 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let statusCode = error?.statusCode || 500;
   let message = error?.message || 'Something went wrong!';
 
-  type TErrorSources = { path: string; message: string }[];
   let errorSources: TErrorSources = [
     {
       path: '',
@@ -18,16 +19,36 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     },
   ];
 
+  const HandleZodError = (error: ZodError) => {
+    const statusCode = 400;
+    const errorSources: TErrorSources = error.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue?.path?.length - 1],
+        message: issue?.message,
+      };
+    });
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSources,
+    };
+  };
+
   if (error instanceof ZodError) {
-    statusCode = 400;
-    message = 'Ami Zod Error';
+    const simplifiesError = HandleZodError(error);
+    // errorSources
+
+    statusCode = simplifiesError?.statusCode;
+    message = simplifiesError?.message;
+    errorSources = simplifiesError?.errorSources;
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error,
+    // error,
     errorSources,
+    stack: config.NODE_ENV === 'development' ? error?.stack : null,
   });
 };
 
