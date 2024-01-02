6d70bc22808b8fa77a8931d5b496fca04ae9c104
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-01-02 09:37:46 +0600

    modifing global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,26 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+// if we use ErrorRequestHandler type we no need to declare the type of those {any Request Response NextFunction}
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  // console.log({ error });
+  let statusCode = error?.statusCode || 500;
+  let message = error?.message || 'Something went wrong!';
 
+  type TErrorSources = { path: string; message: string }[];
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
 
-  console.log({ error });
-  const statusCode = error?.statusCode || 500;
-  const message = error?.message || 'Something went wrong!';
-
+  if (error instanceof ZodError) {
+    statusCode = 400;
+    message = 'Ami Zod Error';
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
     error,
+    errorSources,
   });
 };
 
 export default globalErrorHandler;         
+
+
+//pattern 
+/*
+success
+message
+errorSources : [
+  path: "",
+  message: "Something"
+]
+stack
+*/
